e735f3072c67b0eb02c892bf6ca3ba5f501a4aee
Survivorage <hzstom@163.com>
2020-10-19 15:44:11 +0800

    适配Python3.8

--- Inform.py
+++ Inform.py
@@ -1,20 +1,19 @@
-#coding = utf-8
-import os
-import configparser
-import sendmail
+# coding = utf-8
 import json
 import inform_mail
 
+
 def read_userdata_json():
     json_filename = 'user_data_test.json'
-    with open(json_filename,encoding='UTF-8') as f:
-        user_data = json.load(f)
-    return user_data
+    with open(json_filename, encoding='UTF-8') as f:
+        user_data2 = json.load(f)
+    return user_data2
 
-fs = open('Inform.txt',encoding='UTF-8')
+
+fs = open('Inform.txt', encoding='UTF-8')
 inform_content = fs.read()
 email = inform_mail.mail()
 user_data = read_userdata_json()
 for user in user_data:
     e_mail = user['mail']
-    email.mail(inform_content,e_mail)
+    email.mail(inform_content, e_mail)
