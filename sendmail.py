46b2dad06c413916121450f6343cfd3d50de19ab
Survivorage <hzstom@163.com>
2020-10-19 15:48:04 +0800

    适配Python3.8

--- sendmail.py
+++ sendmail.py
@@ -1,46 +1,39 @@
-#coding=utf-8
+# coding=utf-8
 
-import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 from smtplib import SMTP_SSL
 import os
 import configparser
 
+
 class mail:
     def __init__(self):
-        proDir = os.path.split(os.path.realpath(__file__))[0]
-        configPath = os.path.join(proDir,"config.ini")
+        ProDir = os.path.split(os.path.realpath(__file__))[0]
+        ConfigPath = os.path.join(ProDir, "config.ini")
         parser = configparser.ConfigParser()
-        parser.read(configPath,encoding='UTF-8')
-        send_mail = {}
+        parser.read(ConfigPath, encoding='UTF-8')
         send_mail = parser.items('sendmail')
         self.host_server = send_mail[0][1]
         self.sender_qq = send_mail[1][1]
         self.pwd = send_mail[2][1]
-        self.sender_qq_mail = send_mail[3][1]
-<<<<<<< HEAD
+        self.sender_qq_mail = self.sender_qq + '@qq.com'
         self.mail_title = send_mail[4][1]
-=======
->>>>>>> 02c8b32c58e3004b08e53c1971b8628d2dea487e
-    def mail(self,str,e_mail):
-    #qq邮箱smtp服务器
-        
-        #收件人邮箱
+
+    def mail(self, str, e_mail):
+        # qq邮箱smtp服务器
+
+        # 收件人邮箱
         receiver = e_mail
 
-        #邮件的正文内容
+        # 邮件的正文内容
         mail_content = str
-        #邮件标题       若需要修改自行设置
-<<<<<<< HEAD
-        mail_title = self.mail_title   
-=======
-        mail_title = 'ZZU健康打卡情况'    
->>>>>>> 02c8b32c58e3004b08e53c1971b8628d2dea487e
+        # 邮件标题       若需要修改自行设置
+        mail_title = 'ZZU健康打卡情况'
 
-        #ssl登录
+        # ssl登录
         smtp = SMTP_SSL(self.host_server)
-        #set_debuglevel()是用来调试的。参数值为1表示开启调试模式，参数值为0关闭调试模式
+        # set_debuglevel()是用来调试的。参数值为1表示开启调试模式，参数值为0关闭调试模式
         smtp.set_debuglevel(1)
         smtp.ehlo(self.host_server)
         smtp.login(self.sender_qq, self.pwd)
