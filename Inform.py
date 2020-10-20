74c196368ea1e89001e9c9965a4ec76e6879b224
Francisblue <852662367@qq.com>
2020-10-20 08:51:17 +0800

    解决超多用户情况下提示上传健康码功能失效

--- Inform.py
+++ Inform.py
@@ -4,9 +4,10 @@ import configparser
 import sendmail
 import json
 import inform_mail
+import time
 
 def read_userdata_json():
-    json_filename = 'user_data_test.json'
+    json_filename = 'user_data.json'
     with open(json_filename,encoding='UTF-8') as f:
         user_data = json.load(f)
     return user_data
@@ -16,5 +17,6 @@ inform_content = fs.read()
 email = inform_mail.mail()
 user_data = read_userdata_json()
 for user in user_data:
+    time.sleep(60)
     e_mail = user['mail']
     email.mail(inform_content,e_mail)
